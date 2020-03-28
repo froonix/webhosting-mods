f894c5446abcf4e21eca663a0cfa8063a0968f15
Christian Schrötter <cs@fnx.li>
2020-03-29 00:30:10 +0100

    RTFM! ;-)
    https://docs.nextcloud.com/server/18/admin_manual/configuration_server/config_sample_php_parameters.html#multiple-config-php-file

---
+++ httpdocs/nextcloud/nextcloud/config/data.config.php
@@ -0,0 +1,6 @@
+<?php
+
+$CONFIG =
+[
+	'datadirectory' => realpath(__dir__ . '/../data'),
+];
--- httpdocs/nextcloud/scripts/cronjob.sh
+++ httpdocs/nextcloud/scripts/cronjob.sh
@@ -2,6 +2,5 @@
 set -euf -o pipefail
 cd "$(dirname "$0")"
 
-./patch-config.sh && \
-../wrapper/cron && \
-exit 0; exit 1
+../wrapper/cron \
+&& exit 0; exit 1
