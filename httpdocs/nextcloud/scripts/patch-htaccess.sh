4e3ca3375b73f7fe7888dc1217f9671dde13e9aa
Christian Schrötter <cs@fnx.li>
2020-04-10 18:26:03 +0200

    Add RewriteRule for favicon.ico

--- httpdocs/nextcloud/scripts/patch-htaccess.sh
+++ httpdocs/nextcloud/scripts/patch-htaccess.sh
@@ -2,5 +2,5 @@
 set -euf -o pipefail
 file="$(dirname "$0")/../nextcloud/.htaccess"
 sed -i -r 's~^(  RewriteRule . index.php.+)$~  RewriteCond %{REQUEST_FILENAME} !/pico.php\n\1~' "$file"
-sed -i -r 's~^(  Options -MultiViews)$~\1\n  RewriteRule ^(sites($|/[^/]+))$ $1/ [L,R]\n  RewriteRule ^sites/ pico.php [L]~' "$file"
+sed -i -r 's~^(  Options -MultiViews)$~\1\n  RewriteRule ^favicon\.ico$ /apps/theming/favicon [L,R]\n  RewriteRule ^(sites($|/[^/]+))$ $1/ [L,R]\n  RewriteRule ^sites/ pico.php [L]~' "$file"
 echo -e "\n# File patched by sed substitute script.\n# Added lines: pico.php and sites rules.\n" >> "$file"
