8dc81931fb4cdbb4c38741a67c3ab17d0a9c270b
Christian Schrötter <cs@fnx.li>
2020-05-06 07:42:28 +0200

    Fix HTTP error 503 at Pico CMS

--- httpdocs/nextcloud/nextcloud/pico.php
+++ httpdocs/nextcloud/nextcloud/pico.php
@@ -6,6 +6,12 @@ if(substr($_SERVER['REQUEST_URI'], 0, 9) === '/pico.php')
 	exit;
 }
 
+# Workaround: https://github.com/nextcloud/cms_pico/issues/125
+foreach(['PHP_SELF', 'SCRIPT_FILENAME', 'SCRIPT_NAME'] as $key)
+{
+	$_SERVER[$key] = str_replace('/pico.php', '/index.php', $_SERVER[$key]);
+}
+
 $site = preg_replace('#^/sites/#', '', $_SERVER['REQUEST_URI']);
 $site = preg_match('#^($|\?)#', $site) ? 'default-site' : $site;
 $_SERVER['REQUEST_URI'] = '/apps/cms_pico/pico_proxy/' . $site;
